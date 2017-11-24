2dd8410331a7f738ce87a0277ce82d7861f63f3e
Lokuxus <lokuxus@hotmail.com>
2017-11-24 17:26:07 -0200

    Começo Montro A, continuar em sala

---
+++ src/Agentes/Monstro_A.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Agentes;
+import jade.core.*;
+
+/**
+ *
+ * @author lokux
+ */
+public class Monstro_A extends Agent{
+    int posX=0;
+    int posY = (int) ((Math.random()*100)%5);
+}
