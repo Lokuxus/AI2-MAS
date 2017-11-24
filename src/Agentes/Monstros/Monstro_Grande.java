ad7c77ed02f985f85e4707740e8a086d04163534
Lokuxus <lokuxus@hotmail.com>
2017-11-24 21:43:34 -0200

    Monstros

---
+++ src/Agentes/Monstros/Monstro_Grande.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Agentes.Monstros;
+
+import jade.core.*;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+
+/**
+ *
+ * @author lokux
+ */
+public class Monstro_Grande extends Agent {
+
+    int posX = 0;
+    int posY = (int) ((Math.random() * 100) % 5);
+    int vida = 15;
+    int ataque = 5;
+    int velocidade = 1;
+
+    protected void setup() {
+        addBehaviour(new CyclicBehaviour(this) {
+
+            @Override
+            public void action() {
+                ACLMessage msga = myAgent.receive();
+                if (msga != null) {
+                    String content = msga.getContent();
+                    vida -= Integer.getInteger(content);
+                    if (vida <= 0) {
+                       doDelete();
+                    }
+                }
+
+                if (posX < 30) {
+                    posX += velocidade;
+
+                } else {
+                    ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                    msg.addReceiver(new AID("Muralha", AID.ISLOCALNAME));
+                    msg.setLanguage("Português");
+                    msg.setOntology("Ataque");
+                    msg.setContent(String.valueOf(ataque));
+                    myAgent.send(msg);
+                }
+            }
+        });
+
+    }
+
+}
